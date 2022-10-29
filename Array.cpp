ac1c6aae7646746965953dae9ee324cbdb8eaedf
Syed <syedtathahiruddin@gmail.com>
2022-10-29 20:11:24 +0530

    completed array stl

--- Array.cpp
+++ Array.cpp
@@ -9,6 +9,18 @@ int main (){
 
     array<int,4> a = {1,2,3,4};
 
+    int size = a.size();
+
+    for(int i = 0;i<size;i++){
+        cout<<a[i]<<endl;
+    }
+  
+    cout<<"Element at 2nd index : "<<a.at(2)<<endl;
+    cout<<"Empty or not : "<<a.empty()<<endl;
+
+    cout<<"Element at front : "<<a.front()<<endl;
+    cout<<"Element at Back : "<<a.back()<<endl;
+
 
     
 }
