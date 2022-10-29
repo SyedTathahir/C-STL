430e060bcb6f4c4078b33189784286c03a882ab6
Syed <syedtathahiruddin@gmail.com>
2022-10-29 20:17:58 +0530

    added pushback

--- vector.cpp
+++ vector.cpp
@@ -7,6 +7,13 @@ int main (){
 
     cout<<"Size-> "<<v.capacity()<<endl;
 
+    v.push_back(1);
+    cout<<"Size-> "<<v.capacity()<<endl;
 
+     v.push_back(2);
+    cout<<"Size-> "<<v.capacity()<<endl;
+
+     v.push_back(3);
+    cout<<"Size-> "<<v.capacity()<<endl;
 
 }
