15467a547ace8aa65b06e88167b9f7a5660af207
Syed <syedtathahiruddin@gmail.com>
2022-10-29 20:45:48 +0530

    added pushback and pushfront

--- deque.cpp
+++ deque.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
-
+#include<deque>
 using namespace std;
 
 int main (){
 
+deque<int> d;
+
+d.push_back(1);
+d.push_front(2);
+
+for(int i:d){
+    cout<<i<<" ";
+}cout<<endl;
 
 
-    
 }
