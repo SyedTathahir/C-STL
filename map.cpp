9097de4890509fed7a152607792643ac8640cbd5
Syed <syedtathahiruddin@gmail.com>
2022-10-30 14:36:26 +0530

    completed map

--- map.cpp
+++ map.cpp
@@ -11,9 +11,28 @@ m[1]= "syed";
 m[12]= "amaan";
 m[3]= "uddin";
 
+m.insert({5,"bheem"});
+
+cout<<"before erase : "<<endl;
 for(auto i: m){
-    cout<<i.first<<endl;
+    cout<<i.first<<i.second<<endl;;
+}cout<<endl;
+
+cout<<"finding 12 : "<<m.count(12)<<endl;
+cout<<"finding -12 : "<<m.count(-12)<<endl;
+m.erase(12);
+cout<<"after erase: "<<endl;
+
+for(auto i: m){
+    cout<<i.first<<i.second<<endl;
+}cout<<endl;
+
+auto it = m.find(5);
+for(auto i = it;i!=m.end();i++){
+    cout<<(*i).first<<endl;
 }
 
+
 }
+
 
