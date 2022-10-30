9d51a18cacdda2970c6fb64e00216527473568a9
Syed <syedtathahiruddin@gmail.com>
2022-10-30 13:35:25 +0530

    completed stack with push,pop,size,empty

--- stack.cpp
+++ stack.cpp
@@ -4,6 +4,16 @@ using  namespace std;
 
 int main(){
 
+stack<string> s;
 
-    
+s.push("syed ");
+s.push("amaan");
+
+cout<<"top element: "<<s.top()<<endl;
+s.pop();
+cout<<"top element: "<<s.top()<<endl;
+cout<<"size : "<<s.size()<<endl;
+cout<<"empty or not : "<<s.empty()<<endl;
+
+
 }
