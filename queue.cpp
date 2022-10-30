932f7973f5a941fff9e2b4e81c09d83963e68395
Syed <syedtathahiruddin@gmail.com>
2022-10-30 13:56:16 +0530

    added push,pop,size and completed queue

--- queue.cpp
+++ queue.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int main (){
 
+    queue<string> q;
+
+    q.push ("syed");
+    q.push ("amaan");
     
+    cout<<"first element "<<q.front()<<endl;
+    cout<<"size before  pop "<<q.size()<<endl;
+
+    q.pop();
+    cout<<"first element "<<q.front()<<endl;
+
+    cout<<"size after pop "<<q.size()<<endl;
+
 }
