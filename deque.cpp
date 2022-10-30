4d2611ee1627c2a8224bde5e7693ad58b7119c53
Syed <syedtathahiruddin@gmail.com>
2022-10-30 13:15:52 +0530

    added erase

--- deque.cpp
+++ deque.cpp
@@ -24,7 +24,18 @@ for(int i:d){
     cout<<i<<" ";
 }cout<<endl;
 */
+cout<<"Element at first index: "<<d.at(1)<<endl;
+cout<<"front : "<<d.front()<<endl;
+cout<<"Back : "<<d.back()<<endl;
 
+cout<<"Empty : "<<d.empty()<<endl;
 
+cout<<"before erase : "<<d.size()<<endl;
+d.erase(d.begin(),d.begin()+1);
+cout<<"after erase : "<<d.size()<<endl;
+
+for(int i:d){
+    cout<<i<<" ";
+}cout<<endl;
 
 }
