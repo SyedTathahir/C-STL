90ea297863dec5915bb132f84ab0b6d41155bed0
Syed <syedtathahiruddin@gmail.com>
2022-10-30 13:29:07 +0530

    completed list

--- lists.cpp
+++ lists.cpp
@@ -5,7 +5,26 @@ using namespace std;
 int main (){
 
     list<int> l;
+    list<int> n(5,100);
+
+    cout<<"printing  'n': ";
+    for(int i:n){
+        cout<<i<<" ";
+    }cout<<endl;
+
     l.push_back(1);
     l.push_front(2);
+    for(int i: l){
+        cout<<i<<" ";
+    }cout<<endl;
+
+    l.erase(l.begin());
+    cout<<"After erase: "<<endl;
+    for(int i: l){
+        cout<<i<<" ";
+    }cout<<endl;
+    
+    cout<<"size of list: "<<l.size()<<endl;
+
 }
 
